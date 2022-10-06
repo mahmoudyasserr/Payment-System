c0adbb41b20f295fbfc579ec2b7a29752374e014
Mahmoud Yasser <84890294+mahmoudyasserr@users.noreply.github.com>
2022-10-06 21:52:10 +0200

    Update server.h

--- Server/server.h
+++ Server/server.h
@@ -22,10 +22,12 @@
 /*******************************************************************************
  *                              Defined Types                                  *
  *******************************************************************************/
+
 typedef enum EN_transState_t {
 	APPROVED,
 	DECLINED_INSUFFECIENT_FUND,
 	DECLINED_STOLEN_CARD,
+	FRAUD_CARD,
 	INTERNAL_SERVER_ERROR
 } EN_transState_t;
 
@@ -41,57 +43,75 @@ typedef enum EN_serverError_t {
 	SAVING_FAILED,
 	TRANSACTION_NOT_FOUND,
 	ACCOUNT_NOT_FOUND,
-	LOW_BALANCE
+	LOW_BALANCE,
+	BLOCKED_ACCOUNT
 } EN_serverError_t;
+
+typedef enum EN_accountState_t {
+	RUNNING, BLOCKED
+} EN_accountState_t;
 
 typedef struct ST_accountsDB_t {
 	float balance;
+	EN_accountState_t state;
 	uint8_t primaryAccountNumber[20];
 } ST_accountsDB_t;
 
 /*******************************************************************************
  *                      	Functions Prototypes                               *
  *******************************************************************************/
-
 /*
  * Function Name: isValidAccount
  * Description: This function will take card data and validate these data.
  * It checks if the PAN exists or not in the server's database.
- * If the PAN doesn't exist will return DECLINED_STOLEN_CARD, else will return SERVER_OK
+ * If the PAN doesn't exist will return ACCOUNT_NOT_FOUND, else will return SERVER_OK
  */
 EN_serverError_t isValidAccount(ST_cardData_t *cardData);
+
+/*
+ * Function Name: isBlockedAccount
+ * Description: This function will check whether an account is blocked or not
+ * If the account is blocked, it will return BLOCKED_ACCOUNT
+ * else it will return SERVER_OK
+ */
+EN_serverError_t isBlockedAccount(ST_accountsDB_t *accountRefrence);
 
 /*
  * Function Name: isAmountAvailable
  * Description: This function will take terminal data and validate these data.
  * It checks if the transaction's amount is available or not.
  * If the transaction amount is greater than the balance in the database will return LOW_BALANCE,
- * else will return OK
+ * else will return SERVER_OK
  */
 EN_serverError_t isAmountAvailable(ST_terminalData_t *termData,
 		ST_cardData_t *cardData);
 
 /*
  * Function Name: saveTransaction
- * Description: This function will take all transaction data into the transactions database.
+ * Description: This function will store all transaction data in the transactions database.
  * It gives a sequence number to a transaction,
- * this number is incremented once a transaction is processed into the server.
- * It saves any type of transaction, APPROVED or DECLINED,
+ * this number is incremented once a transaction is processed into the server,
+ * you must check the last sequence number in the server
+ * to give the new transaction a new sequence number.
+ * It saves any type of transactions, APPROVED or DECLINED,
  * with the specific reason for declining/transaction state.
- * If transaction can't be saved will return SAVING_FAILED, else will return OK
+ * If the transaction can't be saved, will return SAVING_FAILED,
+ * else will return SERVER_OK,
+ * It checks if the transaction is saved or not using the getTransaction function.
  */
+
 EN_serverError_t saveTransaction(ST_transaction_t *transData);
 
 /*
- * Function Name: receiveTransactionData
- * Description: This function will take all transaction data and validate its data.
+ * This function will take all transaction data and validate its data.
  * It checks the account details and amount availability.
- * If the account does not exist return DECLINED_STOLEN_CARD,
+ * If the account does not exist return FRAUD_CARD,
  * if the amount is not available will return DECLINED_INSUFFECIENT_FUND,
+ * if the account is blocked will return DECLINED_STOLEN_CARD,
  * if a transaction can't be saved will return INTERNAL_SERVER_ERROR and will not save the transaction,
  * else returns APPROVED.
  * It will update the database with the new balance.
- */
+*/
 EN_transState_t receiveTransactionData(ST_transaction_t *transData);
 
 /*
